af53ffc9cc88056692729c833501f78e47e8c833
mrivs <valentinilyins@gmail.com>
2022-12-27 17:27:16 +0300

    h4 t3 added

--- homework04/task03.py
+++ homework04/task03.py
@@ -1,1 +1,20 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+def get_one(array):
+    dict = {}
+    list1 = []
+    for i in range(len(array)):
+        if dict.get(array[i]) != None:
+            dict[array[i]] = dict[array[i]]+1
+        else:
+            dict[array[i]] = 1
+
+    for key in dict:
+        if dict[key] == 1:
+            list1.append(key)
+
+    return (list1)
+
+
+array = ["2 psa", 33, 33, 33, 22, 22, 1, "kot", "2 psa"]
+print(get_one(array))
