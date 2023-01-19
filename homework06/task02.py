ba906c9468660a148b7785b29c4f490438bd88f4
mrivs <valentinilyins@gmail.com>
2023-01-19 22:54:23 +0300

    homework06  task03 added

--- homework06/task02.py
+++ homework06/task02.py
@@ -1,6 +1,6 @@
 # 45. Найти сумму чисел списка стоящих на нечетной позиции
 
-array=[2,3,5,9,3,7]
+array=[2,3,5,9,1]
 summ=lambda li:sum([v for i,v in enumerate(li) if i%2!=0])
 print(summ (array))
 
