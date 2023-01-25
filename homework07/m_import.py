2d2ff3afed515fc9f6c7da6d7b6f26711cf4b3ed
mrivs <valentinilyins@gmail.com>
2023-01-26 01:24:35 +0300

    gitignore added

---
+++ homework07/m_import.py
@@ -0,0 +1,13 @@
+# модуль импорта
+
+def read_data():
+    with open ("homework07/database.txt","r", encoding="utf8" ) as data:
+        
+        li=data.readlines()
+        array= [st.split() for st in li]
+    return array
+
+def print_data(): 
+    with open ("homework07/database.txt","r", encoding="utf8" ) as data:
+        print(data.read())
+        
--- homework07/view.py
+++ homework07/view.py
@@ -1,1 +1,6 @@
 # модуль вывода
+import m_import
+
+def view_names():
+    array=m_import.read_data()
+    for line in array: print(line[1]+" "+line[2])
