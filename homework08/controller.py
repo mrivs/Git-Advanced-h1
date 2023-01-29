6eeb677bbc960921187a1807fd98f997c3b9cd1d
mrivs <valentinilyins@gmail.com>
2023-01-29 22:11:09 +0300

    homework 08 compl added

--- homework08/controller.py
+++ homework08/controller.py
@@ -10,7 +10,7 @@
 # Достаточно хранить данные в переменной
 
 # Доп*:
-# 1) Добавить функцию генерации сразу ста учеников и записи их в журнал
+# 1) Добавить функцию генерации сразу ста  учеников и записи их в журнал
 # (имя - рандомное из списка нескольких имен
 # фамилия - рандомная из списка нескольких фамилий
 # название предмета - рандом из списка с предметами
@@ -19,14 +19,13 @@
 # 3) Вывод среднего балла по школе по конкретному предмету
 # 4) Вывод количества учеников претендующих на золотую медаль (все оценки либо 4 либо 5)
 # Добавить хранение в файле, и импорт из файла
-import imports
-import view
+
 import interface
 import database
 import random
 scdata={}
 students=["кот"]
-lessons=["мат"]
+lessons=["матан"]
 for st in students:scdata[st]={}
 for name in students:
                for l in lessons: scdata[name][l]=[]
@@ -55,34 +54,36 @@ def start():
     while(True):
         print(interface.menu)
         n=int(input("введите команду: "))
-        if n==1:
+        if n==1:                            # 1 Добавление нового студента (Поля - имя, фамилия)
             name=interface.input_student()
             if not add_student(name):print("имя уже есть!")
 
-        elif n==2:  
+        elif n==2:                          # 2 Добавление предмета (добавляется всем ученикам сразу)
             lesson=interface.input_lesson()
             if not add_lesson(lesson):print("предмет уже есть!")
        
-        elif n==3:
+        elif n==3:                          # 3 Добавление оценки ученику по предмету
             name,lesson,mark=interface.input_mark()
-            scdata[name][lesson].append(mark)
+            if name not in students or lesson not in lessons: print("Неправильный запрос")
+            else: scdata[name][lesson].append(mark)
             
-        elif n==4:
+            
+        elif n==4:                          # 4 Показ списка учеников (имена фамилия)
             for stud, less in scdata.items(): 
                 print (stud,less)
 
-        elif n==5:
+        elif n==5:                          # 5 Показ листа оценок конкретного ученика
             name=interface.input_student()
             print(scdata[name])
 
         elif n==6:
             break
-        elif n==7: 
+        elif n==7:                          # Доп*:
             print(interface.submenu)
             n=int(input("введите команду: "))
-            if n==1: # заполняем базу ранд. учениками предметами и оценками
+            if n==1:                        # заполняем базу ранд. учениками предметами и оценками
                 k=0
-                while (k<10):
+                while (k<100):
                     name=database.get_rand_name()
                     if add_student(name): k+=1
                 k=0
@@ -92,9 +93,9 @@ def start():
                 for name in students:
                     for lesson in lessons:
                         for i in range(1,4):    
-                            scdata[name][lesson].append(random.randint(3,5)) # без двоек
+                            scdata[name][lesson].append(random.randint(3,5)) # оценки без двоек
 
-            elif n==2: 
+            elif n==2:                      # 2) Вывод средней оценки ученика по одному предмету
                 name=interface.input_student()
                 lesson=interface.input_lesson()
                 if name not in students or lesson not in lessons: print("Неправильный запрос")
@@ -103,15 +104,27 @@ def start():
                     sr=float (sum(line)/len(line))
                     print (f"Средняя оценка ученика {name} по предмету {lesson}: {sr:.2f}")
 
-            elif n==3:
-
+            elif n==3:                      # 3) Вывод среднего балла по школе по конкретному предмету
+                lesson=interface.input_lesson()
+                if lesson not in lessons: print("Неправильный запрос")
+                else:
+                    line=[]
+                    for name in students:
+                        line=line+scdata[name][lesson]
+                    sr=float (sum(line)/len(line))
+                    print (f"Средняя оценка по предмету {lesson}: {sr:.2f}")
                 
-                view.show_sr_all()
-            elif n==4:
-                view.show_gold()
+            elif n==4:                      # 4) Вывод количества учеников претендующих на золотую медаль (все оценки либо 4 либо 5)
+                gold=0
+                for name in students:
+                    isgold=True
+                    for lesson in lessons: 
+                        line=scdata[name][lesson]
+                        if 2 in line or 3 in line:
+                            isgold=False
+                    if isgold==True: gold +=1         
+                print (f"претендующих на золотую медаль: {gold}")        
             
             elif n==6:
                 break
             
-
-
