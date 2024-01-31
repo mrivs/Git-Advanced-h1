b91251872a00bfa8f4441f4a8ae924e0196a2cab
mrivs <valentinilyins@gmail.com>
2024-01-31 20:13:44 +0300

    delete homeworks

--- homework05/task04.py
+++
@@ -1,7 +0,0 @@
-# 38. Напишите программу, удаляющую из текста все слова, содержащие ""абв"".(Задание из семинара)
-
-
-text = "ааабваа! аааа, аабв вввв. Абв ггг"
-text_list = text.split()
-result = list(filter(lambda x: not "абв" in x.lower() ,text_list))
-print(result)
--- homework07/view.py
+++
@@ -1,6 +0,0 @@
-# модуль вывода
-import m_import
-
-def view_names():
-    array=m_import.read_data()
-    for line in array: print(line[1]+" "+line[2])
